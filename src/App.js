c7ef32ee864c5c789018d88f88c37f0366cb777c
davegahn <anxieter@gmail.com>
2019-06-24 13:06:54 +0300

    Deploy to firebase

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import './App.css';
 
 import { Container, Alert } from 'reactstrap';
 import { login } from './api/actions';
 import Invite from './invite';
-import Profile from './Profile';
+import Profile from './profile';
 
 const API_VERSION = '5.73';
+const APP_KEY = 6262771;
+// Этого можно было совсем не делать,
+// просто решил показать умение использовать react.context
+const LoginContext = createContext({});
+const UserProvider = LoginContext.Provider;
+export const UserConsumer = LoginContext.Consumer;
 
 class App extends React.Component {
   validateToken = () => {
@@ -19,35 +25,41 @@ class App extends React.Component {
     errorOccurred: false,
   };
 
+  /**
+   * Аутентифицируемся черз VK API и сохраняем ключ в localStorage
+   */
   handleLogin = () => {
-    login(6262771, 2)
+    login(APP_KEY, 2)
       .then(response => {
         const token = response.session.sid;
         localStorage.setItem('access_token', token);
         this.setState({ isAuthorized: true, errorOccurred: false });
       })
       // Здесь я решил перхватить ошибку из reject чтобы отреденерить сообщение
+      // если логин не успешен
       .catch(err => this.setState({ errorOccurred: true }));
   };
 
   render() {
     const { isAuthorized, errorOccurred } = this.state;
     return (
-      <div className="App">
-        <Container>
-          {errorOccurred && (
-            <Alert className="alert-custom" color="danger">
-              Ошибка авторизаци!
-            </Alert>
-          )}
-          {!isAuthorized && <Invite handleAuthButton={this.handleLogin} />}
-          {isAuthorized && <Profile api={API_VERSION} />}
-          <p className="laws">
-            Free Vector Art by{' '}
-            <a href="https://vecteezy.com">www.vecteezy.com</a>
-          </p>
-        </Container>
-      </div>
+      <UserProvider value={this.handleLogin}>
+        <div className="App">
+          <Container>
+            {errorOccurred && (
+              <Alert className="alert-custom" color="danger">
+                Ошибка авторизаци!
+              </Alert>
+            )}
+            {!isAuthorized && <Invite />}
+            {isAuthorized && <Profile api={API_VERSION} />}
+            <p className="laws">
+              Free Vector Art by{' '}
+              <a href="https://vecteezy.com">www.vecteezy.com</a>
+            </p>
+          </Container>
+        </div>
+      </UserProvider>
     );
   }
 }
--- src/invite.js
+++ src/invite.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import { Row, Col, Button } from 'reactstrap';
+import { UserConsumer } from './App';
 
-export default function Invite({ handleAuthButton }) {
-  return (
-    <Row>
-      <Col className="login-container">
-        <h1 className="center-head">
-          Пожалуйста, авторизуйтесь для входа в приложение
-        </h1>
-        <Button color="danger" onClick={() => handleAuthButton()} size="lg">
-          Авторизоваться
-        </Button>
-      </Col>
-    </Row>
-  );
-}
+const Invite = () => (
+  <UserConsumer>
+    {context => {
+      return (
+        <Row>
+          <Col className="login-container">
+            <h1 className="center-head">
+              Пожалуйста, авторизуйтесь для входа в приложение
+            </h1>
+            <Button color="danger" onClick={() => context()} size="lg">
+              Авторизоваться
+            </Button>
+          </Col>
+        </Row>
+      );
+    }}
+  </UserConsumer>
+);
+
+export default Invite;
